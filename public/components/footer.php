6908d435a112a0d23ac3ddf720aab87d438e468c
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-02 14:14:02 +0700

    First commit

---
+++ public/components/footer.php
@@ -0,0 +1,12 @@
+  <script src="../src/js/aos.js"></script>
+  <script src="../src/js/rellax.min.js"></script>
+  <script src="../src/js/lax.min.js"></script>
+  <script>
+    new Rellax('.rellax');
+    AOS.init({
+      duration: 1000
+    });
+  </script>
+  </body>
+
+  </html>
