e4eb20af4d6fead0e56e8d72581cb9d538facc28
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-15 11:16:01 +0700

    feat: Add navbar to header

--- components/header.php
+++ components/header.php
@@ -8,4 +8,16 @@
   <title>Home</title>
 </head>
 
-<body class="bg-neutral-200">
+<body class="bg-gray-200">
+  <nav class="fixed w-screen top-0 bg-gray-50">
+    <div class="flex justify-between content-center max-w-[75%] mx-auto py-2">
+      <a href="#" class="text-2xl font-bold">LOGO</a>
+      <ul class="flex items-center space-x-6 text-sm text-gray-700">
+        <li><a href="#" class="font-semibold hover:text-green-600">HOME</a></li>
+        <li><a href="#" class="font-semibold hover:text-green-600">DESTINATION</a></li>
+        <li><a href="#" class="font-semibold hover:text-green-600">ACTIVITIES</a></li>
+        <li><a href="#" class="font-semibold hover:text-green-600">EVENTS</a></li>
+        <li><a href="#" class="font-semibold hover:text-green-600">CONTACT</a></li>
+      </ul>
+    </div>
+  </nav>
