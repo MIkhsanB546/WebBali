a6d89a4ad5f59c7a008028ffef82b76c55d6bae4
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-15 18:38:54 +0700

    feat: Add about section

--- components/header.php
+++ components/header.php
@@ -9,10 +9,10 @@
 </head>
 
 <body class="bg-gray-200">
-  <nav class="fixed w-screen top-0 bg-gray-50">
-    <div class="flex justify-between content-center max-w-[75%] mx-auto py-2">
+  <nav class="fixed w-screen top-0 z-50 bg-gray-50">
+    <div class="flex justify-between content-center max-w-[85%] mx-auto py-4">
       <a href="#" class="text-2xl font-bold">LOGO</a>
-      <ul class="flex items-center space-x-6 text-sm text-gray-700">
+      <ul class="hidden items-center space-x-6 text-sm text-gray-700 md:flex">
         <li><a href="#" class="font-semibold hover:text-green-600">HOME</a></li>
         <li><a href="#" class="font-semibold hover:text-green-600">DESTINATION</a></li>
         <li><a href="#" class="font-semibold hover:text-green-600">ACTIVITIES</a></li>
