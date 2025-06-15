fe3d1d7e566eb9f1e0e9e7b87f40579a6e790b26
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-15 18:37:43 +0700

    style: Adjust navbar padding

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include_once 'components/header.php' ?>
+
+<header class="h-screen bg-[url(./assets/img/pic2.jpg)] bg-cover bg-center bg-fixed">
+  <div class="flex justify-center items-center h-full bg-black/50">
+    <div class="text-center text-white rellax">
+      <h1 class="mb-4 text-6xl font-bold">Discovery Beautiful Bali Island</h1>
+      <h3 class="mb-8 text-lg">One of the best tourist destination in Indonesia</h3>
+      <a href="#" class="p-2 px-4 transition bg-green-500 text-white rounded hover:bg-green-600">Explore Destinations</a>
+    </div>
+  </div>
+</header>
+
+<!-- todo: fix below and figure out img layout-->
+<section class="h-screen">
+  <div class="grid lg:grid-cols-2 items-center">
+    <p>
+      Bali is the Island of God in Paradise among Indonesian tourism. Furthermore, it is a perfect tourist destination to enjoy the holiday with your family as well as collages. This tropical paradise has a unique blend of modern tourist facilities combined with wonderful shopping, a rich past as well as heritage sites.
+    </p>
+    <img src="./assets/img/pic10.jpg" alt=""
+      class="h-auto justify-center">
+  </div>
+</section>
+
+
+<?php include_once 'components/footer.php' ?>
