0269be00743ccb73db346dc7c1973f55b9c6c247
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-16 11:51:07 +0700

    style: navbar Adjustments

--- components/header.php
+++ components/header.php
@@ -5,14 +5,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <!-- Boxicons CSS -->
+  <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+  <link href='https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css' rel='stylesheet'>
+
   <title>Home</title>
 </head>
 
 <body class="bg-gray-200">
   <nav class="fixed w-screen top-0 z-50 bg-gray-50">
     <div class="flex justify-between content-center max-w-[85%] mx-auto py-4">
-      <a href="#" class="text-2xl font-bold">LOGO</a>
-      <ul class="hidden items-center space-x-6 text-sm text-gray-700 md:flex">
+      <a href="#" class="text-2xl font-bold text-gray-800">WebBali</a>
+      <ul class="hidden items-center space-x-6 text-sm text-gray-800 md:flex">
         <li><a href="#" class="font-semibold hover:text-green-600">HOME</a></li>
         <li><a href="#" class="font-semibold hover:text-green-600">DESTINATION</a></li>
         <li><a href="#" class="font-semibold hover:text-green-600">ACTIVITIES</a></li>
