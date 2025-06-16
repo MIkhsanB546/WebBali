1c32f854d476355f9a824aefe060936b0b73f702
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-16 11:49:22 +0700

    feat: Add about section

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php include_once 'components/header.php' ?>
 
-<header class="h-screen bg-[url(./assets/img/pic2.jpg)] bg-cover bg-center bg-fixed">
+<header class="h-screen bg-[url(./assets/img/pic2.jpg)] bg-cover bg-center bg-fixed" id="hero">
   <div class="flex justify-center items-center h-full bg-black/50">
     <div class="text-center text-white rellax">
       <h1 class="mb-4 text-6xl font-bold">Discovery Beautiful Bali Island</h1>
-      <h3 class="mb-8 text-lg">One of the best tourist destination in Indonesia</h3>
+      <p class="mb-8 text-lg">One of the best tourist destination in Indonesia</p>
       <a href="#" class="p-2 px-4 transition bg-green-500 text-white rounded hover:bg-green-600">Explore Destinations</a>
     </div>
   </div>
 </header>
 
-<!-- todo: fix below and figure out img layout-->
-<section class="h-screen">
-  <div class="grid lg:grid-cols-2 items-center">
-    <p>
-      Bali is the Island of God in Paradise among Indonesian tourism. Furthermore, it is a perfect tourist destination to enjoy the holiday with your family as well as collages. This tropical paradise has a unique blend of modern tourist facilities combined with wonderful shopping, a rich past as well as heritage sites.
-    </p>
-    <img src="./assets/img/pic10.jpg" alt=""
-      class="h-auto justify-center">
+<section class="flex justify-center py-16">
+  <div class="w-3/4 text-justify">
+    <div class="mb-8">
+      <h2 class="text-3xl font-bold text-green-600 mb-4" id="about">
+        About
+      </h2>
+      <p class="mb-4 leading-7">Bali is the Island of God in Paradise among Indonesian tourism. Furthermore, it is a perfect tourist destination to enjoy the holiday with your family as well as collages. This tropical paradise has a unique blend of modern tourist facilities combined with wonderful shopping, a rich past as well as heritage sites.</p>
+      <p class="mb-4 leading-7">The Balinese people are proud of having preserved their unique Hindu culture against the advance of Islam, the dominant religion throughout Indonesia. It is also the Balinese festivals in a magnificent temple as well as palaces.</p>
+      <p class="mb-4 leading-7">We can find some best surfing spots on the island on the western side. Meanwhile, the eastern side is a wonderful haven for families, with beautiful white sand beaches and gentle seas. Once you explore the Indonesian tourism please do not miss to visit Bali.</p>
+    </div>
+  </div>
+  <div class="mb-8">
+    <h2 class="text-3xl font-bold text-green-600 mb-4" id="top_destinations">
+      Top Destinations
+    </h2>
+    <div class="flex"></div>
   </div>
 </section>
+
 
 
 <?php include_once 'components/footer.php' ?>
