ddc54c07f76e356be5950dc5767adb9f25141e04
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-16 11:50:20 +0700

    feat: Add Copyright

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,7 @@
+<footer class="flex justify-center items-center h-22 bg-gray-50">
+  <p class="text-gray-500">Copyright 2025</p>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js"></script>
 <script>
   new Rellax('.rellax');
