e3b558ad3c7da23b86f5ec5de522d81c5f8449f2
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-17 18:28:30 +0700

    feat: Add top destination section

--- index.php
+++ index.php
@@ -1,34 +1,60 @@
 <?php include_once 'components/header.php' ?>
 
-<header class="h-screen bg-[url(./assets/img/pic2.jpg)] bg-cover bg-center bg-fixed" id="hero">
+<header class="h-screen bg-[url(./assets/img/hero.jpg)] bg-cover bg-center bg-fixed" id="hero">
   <div class="flex justify-center items-center h-full bg-black/50">
     <div class="text-center text-white rellax">
       <h1 class="mb-4 text-6xl font-bold">Discovery Beautiful Bali Island</h1>
       <p class="mb-8 text-lg">One of the best tourist destination in Indonesia</p>
-      <a href="#" class="p-2 px-4 transition bg-green-500 text-white rounded hover:bg-green-600">Explore Destinations</a>
+      <a href="#" class="py-2 px-4 transition bg-green-500 text-white rounded hover:bg-green-600">Explore Destinations</a>
     </div>
   </div>
 </header>
 
-<section class="flex justify-center py-16">
-  <div class="w-3/4 text-justify">
-    <div class="mb-8">
-      <h2 class="text-3xl font-bold text-green-600 mb-4" id="about">
-        About
-      </h2>
-      <p class="mb-4 leading-7">Bali is the Island of God in Paradise among Indonesian tourism. Furthermore, it is a perfect tourist destination to enjoy the holiday with your family as well as collages. This tropical paradise has a unique blend of modern tourist facilities combined with wonderful shopping, a rich past as well as heritage sites.</p>
-      <p class="mb-4 leading-7">The Balinese people are proud of having preserved their unique Hindu culture against the advance of Islam, the dominant religion throughout Indonesia. It is also the Balinese festivals in a magnificent temple as well as palaces.</p>
-      <p class="mb-4 leading-7">We can find some best surfing spots on the island on the western side. Meanwhile, the eastern side is a wonderful haven for families, with beautiful white sand beaches and gentle seas. Once you explore the Indonesian tourism please do not miss to visit Bali.</p>
-    </div>
-  </div>
-  <div class="mb-8">
-    <h2 class="text-3xl font-bold text-green-600 mb-4" id="top_destinations">
-      Top Destinations
+<section class="flex items-center py-8 lg:h-screen lg:py-0">
+  <div class="w-5/6 mx-auto text-justify lg:w-3/4">
+    <h2 class="text-5xl font-bold text-green-600 mb-8">
+      About
     </h2>
-    <div class="flex"></div>
+    <p class="mb-6 text-base/8">Bali is the Island of God in Paradise among Indonesian tourism. Furthermore, it is a perfect tourist destination to enjoy the holiday with your family as well as collages. This tropical paradise has a unique blend of modern tourist facilities combined with wonderful shopping, a rich past as well as heritage sites.</p>
+    <p class="mb-6 text-base/8">The Balinese people are proud of having preserved their unique Hindu culture against the advance of Islam, the dominant religion throughout Indonesia. It is also the Balinese festivals in a magnificent temple as well as palaces.</p>
+    <p class="mb-6 text-base/8">We can find some best surfing spots on the island on the western side. Meanwhile, the eastern side is a wonderful haven for families, with beautiful white sand beaches and gentle seas. Once you explore the Indonesian tourism please do not miss to visit Bali.</p>
   </div>
 </section>
 
+<section class="flex items-center py-8 lg:h-screen lg:py-0">
+  <div class="w-5/6 mx-auto text-justify lg:w-3/4">
+    <h2 class="text-5xl font-bold text-green-600 mb-8">
+      Top Destinations
+    </h2>
+    <div class="flex flex-col justify-around gap-6 lg:flex-row">
+      <!-- cards -->
+      <div class="max-w-xs bg-white rounded-lg shadow-md overflow-hidden">
+        <img src="./assets/img/pic1.png" alt="GWK Cultural Park" class="w-full h-48 object-cover">
+        <div class="p-4">
+          <h3 class="text-lg font-semibold mb-2">GWK Cultural Park</h3>
+          <p class="text-gray-600 mb-4">Witness with your very own eyes the beautiful dances like the haunting Kecak.</p>
+        </div>
+      </div>
+      <div class="max-w-xs bg-white rounded-lg shadow-md overflow-hidden">
+        <img src="./assets/img/pic2.png" alt="Ulun Danu Bratan Temple" class="w-full h-48 object-cover">
+        <div class="p-4">
+          <h3 class="text-lg font-semibold mb-2">Ulun Danu Bratan Temple</h3>
+          <p class="text-gray-600 mb-4">the Ulun Danu Bratan Temple is a popular attraction among both tourists and locals.</p>
+        </div>
+      </div>
+      <div class="max-w-xs bg-white rounded-lg shadow-md overflow-hidden">
+        <img src="./assets/img/pic3.png" alt="Tanah Lot" class="w-full h-48 object-cover">
+        <div class="p-4">
+          <h3 class="text-lg font-semibold mb-2">Tanah Lot</h3>
+          <p class="text-gray-600 mb-4">Tanah Lot is an exotic ancient Hindu shrine perched on top of an outcrop amidst constantly crashing waves.</p>
+        </div>
+      </div>
+    </div>
+    <div class="text-center mt-12">
+      <a href="#" class="py-2 px-4 bg-green-500 text-white font-semibold rounded transition hover:bg-green-600">Learn More</a>
+    </div>
+  </div>
+</section>
 
 
 <?php include_once 'components/footer.php' ?>
