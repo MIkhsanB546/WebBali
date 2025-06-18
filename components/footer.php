94d6cea9713d4d2630e175b447b99df1038b22d6
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-18 18:45:52 +0700

    feat: Add copyright symbol ©

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <footer class="mt-auto flex justify-center items-center h-22 bg-gray-50">
-  <p class="text-gray-500">Copyright 2025</p>
+  <p class="text-gray-500">Copyright 2025 © WebBali</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js"></script>
