c6d42132d852b37f407d3efbc7a19a181c20eb73
MIkhsanB546 <mikhsanb546@gmail.com>
2025-06-20 12:14:50 +0700

    feat: Form can send to gmail

--- contact.php
+++ contact.php
@@ -8,15 +8,15 @@ include_once 'components/header.php'
   <div class="max-w-3/4 mx-auto">
     <h2 class="mb-6 font-bold text-2xl text-green-500">CONTACT</h2>
     <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-      <form method="post" class="p-4 bg-gray-100 rounded-lg">
+      <form action="mailto:mikhsanb546+webbali@gmail.com" method="post" enctype="text/plain" class="p-4 bg-gray-100 rounded-lg">
         <div class="mb-4">
           <label for="name" class="">Your Name</label>
           <input type="text" id="name" name="name" placeholder="Your Name"
             class="block p-1 w-full border-2 border-gray-400 rounded focus:border-green-500" required>
         </div>
         <div class="mb-4">
-          <label for="email" class="">Your Email</label>
-          <input type="text" id="email" name="email" placeholder="example@mail.com"
+          <label for="mail" class="">Your Email</label>
+          <input type="text" id="mail" name="mail" placeholder="example@mail.com"
             class="block p-1 w-full border-2 border-gray-400 rounded focus:border-green-500" required>
         </div>
         <div class="mb-4">
@@ -24,7 +24,7 @@ include_once 'components/header.php'
           <textarea type="text" id="message" name="message" rows="3" placeholder=""
             class="block p-1 w-full border-2 border-gray-400 rounded focus:border-green-500" required></textarea>
         </div>
-        <input type="submit" value="Submit" class="py-2 px-4 bg-green-500 font-semibold text-white rounded cursor-pointer hover:bg-green-600">
+        <input type="submit" value="Send" class="py-2 px-4 bg-green-500 font-semibold text-white rounded cursor-pointer hover:bg-green-600">
         <input type="reset" value="Reset" class="py-2 px-4 bg-gray-400 font-semibold text-white rounded cursor-pointer hover:bg-gray-500">
       </form>
       <div class="p-4 bg-gray-100 rounded-lg">
